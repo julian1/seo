be1ec4d50f328593367ae4450f30ea443930e514
julian1 <julian.atkinson71@gmail.com>
2014-12-02 09:47:10 +1100

    tidy

--- example_07/run.sh
+++ example_07/run.sh
@@ -1,6 +1,5 @@
 set -x
 rm output* -rf 2> /dev/null
 java -jar ../saxon9he.jar  ./dummy.xml records-process.xsl
-#java -jar ../saxon9he.jar  ../records/argo_with_wb_and_lm.xml view.xsl 
 cp imos.css output
 tar czf output.tgz output/
